6d0e6ac6172c530397bca772ffd951a1b2dca5d2
粽子521 <1139319562@qq.com>
2016-11-14 10:43:19 +0800

    网络请求

--- app/src/main/java/com/bwie/gituser/MainActivity.java
+++ app/src/main/java/com/bwie/gituser/MainActivity.java
@@ -11,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("xioajiejie");
+        
+        getData();
 
+    }
+
+    private void getData() {
     }
 }
